4d94209b14c640d6ab74ff3ea7e952232e44f03e
Dat Nguyen <datnguyen@onhandbi.com>
2025-05-18 21:34:13 +0700

    initialize

---
+++ src/main/java/com/danny/danloi/DanloiApplication.java
@@ -0,0 +1,13 @@
+package com.danny.danloi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanloiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanloiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danny/danloi/DanloiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danny.danloi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanloiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
